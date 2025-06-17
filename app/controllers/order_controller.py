fd11fe10e703b53aa39b1088f2713f96ed89c1d9
PaulTJ30 <pultj30@gmail.com>
2025-06-17 13:37:32 -0600

    migrations

--- app/controllers/order_controller.py
+++ app/controllers/order_controller.py
@@ -16,6 +16,28 @@ def add_garmen ( order_id, type, description, notes):
     db.session.commit()
     return garment
 
-def create_order_detal (garment_id, service_id, quantity):
+def create_order_detail (garment_id, service_id, quantity):
     order_detail = OrderDetail(garment_id=garment_id, service_id= service_id, quantity= quantity)
-    
+    db.session.add(order_detail)
+    db.session.commit()
+    return order_detail
+
+def update_order_status (order_id, new_status):
+    order = Order.query.get(order_id)
+    if not order:
+        return None
+    order.state = new_status
+    db.session.commit ()
+    return order
+
+def list_orders_by_status(status):
+    orders = Order.query.filter_by(state=status).all()
+    data = [{
+        "id":o.id,
+        "client_id":o.client_id,
+        "state": o.state,
+        "estimated_delivery_date": o.estimated_delivery_date,
+        "total": o.total,
+        "pagado": o.pagado,   
+    } for o in orders]
+    return data
